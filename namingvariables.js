691cd3d0141624ba61cd44d1e6000c05a24c8a27
Sagar1555 <stimalsena155@gmail.com>
2022-07-01 22:41:40 +0545

    naming

---
+++ namingvariables.js
@@ -0,0 +1,18 @@
+//rules for naming variables
+
+//you cannot start with number
+//example:-
+//1value(invalid)
+//value1(valid)
+
+var value1 = 10;
+console.log(value1 **2);
+
+//you can use only underscore _ or dollor symbol $
+//first_name(valid)
+//_first_name(valid)
+
+//you can not use space in variable name
+
+//convention 
+//start with small letter and use camelcase
