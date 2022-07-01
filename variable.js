221bfa7e0cfdfb764dd6fd973932e5fdbfc95f50
Sagar1555 <stimalsena155@gmail.com>
2022-07-01 22:34:46 +0545

    variable

--- variable.js
+++ variable.js
@@ -1,11 +1,17 @@
+"use strict";
+
 //intro to variable
 
 //variable can store some information we can use and change that information later 
 
 //declare a variable
-var firstname="John";
+firstname="John";
 var secondname="Smith";
 
-console.log(firstname);
-console.log(secondname);
-console.log(firstname+" "+secondname);
+// console.log(firstname);
+// console.log(secondname);
+// console.log(firstname+" "+secondname);
+
+//change value
+firstname="John Prasad";
+
