3f685f452448cbd228b2696f419693962650df9b
Sagar1555 <stimalsena155@gmail.com>
2022-07-03 11:05:35 +0545

    copit failed

---
+++ game.js
@@ -0,0 +1,20 @@
+const a=4;
+//write a game in javascript
+//the game is a simple game of tic tac toe
+//the game is played by two players
+//the game is played on a 3x3 board
+//the game is played by the computer
+//the game is played by the human
+//the game is played by the computer
+//the game is played by the human
+//the game is played by the computer
+//the game is played by the human
+//the game is played by the computer
+//the game is played by the human
+//the game is played by the computer    
+//the game is played by the human
+//the game is played by the computer
+//the game is played by the human
+//the game is played by the computer
+//the game is played by the human
+
