475900ffc996d3004e890b235d2bb15d21a38caf
Sagar1555 <stimalsena155@gmail.com>
2022-07-05 07:17:14 +0545

    string method,concatenation,undefined

---
+++ stringconcatination.js
@@ -0,0 +1,17 @@
+//string concatenated 
+
+let string1 = "Hello";
+let string2 = "World";
+
+let newstring = string1 + string2;
+console.log(newstring);
+
+//template string
+let age = 20;
+let firstName ="Saagar"
+
+//let aboutme ="my name is "+ firstName + " and I am " + age + " years old";
+console.log(aboutme);
+
+let aboutme = `my name is ${firstName} and I am ${age} years old`;
+console.log(aboutme)
---
+++ undefined.js
@@ -0,0 +1,28 @@
+//undefined
+//null
+
+// let firstName;
+
+// console.log(typeof firstName);
+
+// firstName ="Saagar Timalsena"
+// console.log(typeof firstName,firstName);
+
+let myvariable = null;
+console.log(typeof myvariable);
+myvariable = "saagar Timalsena";
+console.log(typeof null); //null 
+//bug, error 
+
+//BigInt
+
+let myNumber = BigInt(14);
+let myNumber2 = 12345;
+
+console.log(myNumber); 
+
+consoloe.log(Number.MAX_SAFE_INTEGER);
+console.log(myNumber + myNumber2);
+
+//BigInt -> BigInt number last maa n add garnee
+
