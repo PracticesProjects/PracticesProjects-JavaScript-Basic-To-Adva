beef30a46995238be549af7931c68a3a666db806
Sagar1555 <stimalsena155@gmail.com>
2022-07-06 14:30:13 +0545

    truth and false values

---
+++ truthly.js
@@ -0,0 +1,26 @@
+//truthy and falsy values
+//falsy values
+
+//false 
+//""
+//null
+//undefined 
+//0
+
+//if else condition
+
+let age = 20;
+
+if (age >20){
+    console.log("Age must be less than 20");
+}
+// if (age <20){
+//     console.log("Age must be greater than 20");
+// }
+else{
+    console.log("Age must be 20");
+}
+
+let number =14;
+
+// % = 0
