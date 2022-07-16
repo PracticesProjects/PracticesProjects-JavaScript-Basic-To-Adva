5a1f938fbeb7d7459d545330d292640673b63226
Sagar1555 <stimalsena155@gmail.com>
2022-07-16 20:30:14 +0545

    and or

---
+++ andoroperator.js
@@ -0,0 +1,14 @@
+let firstName ="Saagar"
+let age = 20;
+
+// if(firstName[0] === "S")
+// {
+//     console.log("your name start with 's'")
+// }
+if(firstName[0] === "S" && age > 20) {
+    console.log("your name start with 's' and age is 20")
+}
+else{
+    console.log("your name doesnot start with s and age is not equal to 20")
+    
+}
--- ternary.js
+++ ternary.js
@@ -1,16 +1,20 @@
 //ternary operators
 
 
-let age = 8 ;
+// let age = 8 ;
 
 
-let drink;
+// let drink;
 
-if(age >= 5){
-    drink = "coffee";
-}
-else{
-    drink = "beears";
+// if(age >= 5){
+//     drink = "coffee";
+// }
+// else{
+//     drink = "beears";
 
-}
+// }
+// console.log(drink);
+
+let age = 8;
+let drink = age >=10 ? "coffee" : "beears";
 console.log(drink);
