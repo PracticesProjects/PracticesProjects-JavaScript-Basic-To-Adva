3f62c3fdb22cd2a95c75b2fb61154d8f5b74a1b0
Sagar1555 <stimalsena155@gmail.com>
2022-07-17 10:00:31 +0545

    ifelse

---
+++ ifelseif.js
@@ -0,0 +1,25 @@
+//if
+//else if
+//else
+
+
+let tempInDegree = 20;
+
+if (tempInDegree < 0) {
+    console.log("extremely cold temperature");
+}
+else if(tempInDegree <16   )
+{
+    console.log("it is cold enough");
+}
+else if(tempInDegree >= 16)
+{
+    console.log("it is 16 degrees");
+
+}
+else if(tempInDegree < 45)
+{
+    console.log("it is 45 degrees");
+}
+
+console.log("tempInDegree");
