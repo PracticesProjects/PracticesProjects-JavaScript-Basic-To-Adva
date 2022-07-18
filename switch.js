8fa8cb78dad9606ba9ee78a9528ec984e93373f9
Sagar1555 <stimalsena155@gmail.com>
2022-07-18 10:46:18 +0545

    switch case

--- switch.js
+++ switch.js
@@ -1,30 +1,52 @@
-//switch statement
+// //switch statement
 
-let day = 0;
+// let day = 0;
 
-if(day == 0){
-    console.log("Sunday");
-}
-else if(day == 1){
-    console.log("Monday");
-}
-else if(day == 2){
-    console.log("Tuesday");
-}
-else if(day == 3){
-    console.log("Wednesday");
-}
-else if(day == 4){
-    console.log("Thursday");
-}
-else if(day == 5){
-    console.log("Friday");
-}
-else if (day == 6){
-    console.log("Saturday");
+// if(day == 0){
+//     console.log("Sunday");
+// }
+// else if(day == 1){
+//     console.log("Monday");
+// }
+// else if(day == 2){
+//     console.log("Tuesday");
+// }
+// else if(day == 3){
+//     console.log("Wednesday");
+// }
+// else if(day == 4){
+//     console.log("Thursday");
+// }
+// else if(day == 5){
+//     console.log("Friday");
+// }
+// else if (day == 6){
+//     console.log("Saturday");
 
+// }
+// else
+// {
+//     console.log("Invalid day");
+// }
+
+let day = 2;
+
+switch (day) {
+    case 0:
+        console.log("Sunday");
+    case 1:
+        console.log("Monday");
+    case 2:
+        console.log("Tuesday");
+    case 3:
+        console.log("Wednesday");
+    case 4:
+        console.log("Thursday");
+    case 5:
+        console.log("Friday");
+    case 6:
+        console.log("Saturday");
+    default:
+        console.log("Invalid day");
 }
-else
-{
-    console.log("Invalid day");
-}
+
