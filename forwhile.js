a8d2d01c92c5a636ded73b7c9aaa6a850533586c
Sagar1555 <stimalsena155@gmail.com>
2022-07-23 21:36:46 +0545

    for while loop example

---
+++ forwhile.js
@@ -0,0 +1,25 @@
+//intro to for while loop
+
+//print o to 9
+
+for(let i = 0; i < 10; i++) {
+    console.log(i);
+}
+// console.log("Value of i is ", i);
+
+
+// for loop example
+
+let total = 0;
+
+let num = 10;
+
+for( let i = 0; i < num; i++ ) {
+    total += i;
+}
+console.log(total);
+
+//break keyword
+
+//continue keyword
+
