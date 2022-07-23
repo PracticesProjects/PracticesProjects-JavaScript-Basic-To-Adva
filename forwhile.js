116795289441484d1af0e09db1ae1e511ca96b1a
Sagar1555 <stimalsena155@gmail.com>
2022-07-23 21:42:38 +0545

    do while loop example

--- forwhile.js
+++ forwhile.js
@@ -21,7 +21,20 @@ console.log(total);
 
 //break keyword
 
-
+for( let i = 0; i <=10; i++ )
+{
+    if( i == 4 )
+    {
+        break;
+    }
+    console.log(i);
+}
 
 //continue keyword
 
+for (let i = 0; i <= 10; i++) {
+  if (i == 4) {
+    continue;
+  }
+  console.log(i)
+}
