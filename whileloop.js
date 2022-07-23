0e052fc4a00d5cfdb5577ace40c4fc3938239d3e
Sagar1555 <stimalsena155@gmail.com>
2022-07-23 21:22:13 +0545

    while loop

--- whileloop.js
+++ whileloop.js
@@ -1,16 +1,20 @@
 //while loop
 
-let i = 0;
-// console.log(i);
-// i++;
-// console.log(i);
-while (i <= 10) {
-    console.log(i);
-    i++;
-}
+// let i = 0;
+//  console.log(i);
+//  i++;
+//  console.log(i);
+// while (i <= 10) {
+//     console.log(i);
+//     i++;
+// }
  
-console.log('Current value of i is ${i}');
+// console.log('Current value of i is ${i}');
 
-let num = 10;
+// let num = 10;
 
-console.log('current value')
+// console.log('current value')
+
+let num = 20;
+let total = (num*(num+1))/2;
+console.log('total value: ${total}');
