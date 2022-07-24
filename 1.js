01b18f3e4baa2a17af9909b723fd2ba95399d518
Sagar1555 <stimalsena155@gmail.com>
2022-07-24 20:20:38 +0545

    clone

--- 1.js
+++ 1.js
@@ -3,4 +3,8 @@
 //how to concatenated two arrays
 let array1 =["item1", "item2"];
 let array2 = array1;
+
+array1.push(["item1", "item2","item3","item4"]);
 console.log( array1===array2 );
+
+console.log(array1);
